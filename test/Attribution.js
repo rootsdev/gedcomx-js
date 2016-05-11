c89a0df14897a6cdea9116bd95aec705c1641e82
Justin <justincyork@gmail.com>
2016-05-11 18:51:56 +0000

    Finish testing Attribution

--- test/Attribution.js
+++ test/Attribution.js
@@ -1,5 +1,6 @@
 var assert = require('chai').assert,
-    Attribution = require('../src/Attribution');
+    Attribution = require('../src/Attribution'),
+    ResourceReference = require('../src/ResourceReference');
 
 describe('Attribution', function(){
   
@@ -23,6 +24,21 @@ describe('Attribution', function(){
     assert.equal(attr.getCreated().getTime(), 1111338494969, 'Created date not saved when created with JSON');
     assert.equal(attr.getCreator().getResource(), 'https://myapp.com/creator', 'Creator not saved when created with JSON');
     assert.equal(attr.getModified().getTime(), 1111338494969, 'Modified date not saved when created with JSON');
+  });
+  
+  it('Create with mixed data', function(){
+    var attr = Attribution({ 
+      changeMessage: 'It changed',
+      contributor: ResourceReference({ resource: 'https://myapp.com/contributor'}),
+      created: new Date(1111338494969),
+      creator: ResourceReference({ resource: 'https://myapp.com/creator'}),
+      modified: new Date(1111338494969)
+    });
+    assert.equal(attr.getChangeMessage(), 'It changed', 'Change message not saved properly when created with mixed data');
+    assert.equal(attr.getContributor().getResource(), 'https://myapp.com/contributor', 'Contributor not saved when created with mixed data');
+    assert.equal(attr.getCreated().getTime(), 1111338494969, 'Created date not saved when created with mixed data');
+    assert.equal(attr.getCreator().getResource(), 'https://myapp.com/creator', 'Creator not saved when created with mixed data');
+    assert.equal(attr.getModified().getTime(), 1111338494969, 'Modified date not saved when created with mixed data');
   });
   
 });
